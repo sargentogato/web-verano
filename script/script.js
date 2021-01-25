85d236c5cea29cdb728dcb1afa69f27d8a85edda
Marlon Velasquez <marlonjvelasquez@gmail.com>
2021-01-25 14:28:21 +0100

    script.js Refactor Delete -some functions -console.log
    Rename some const to better readability

--- script/script.js
+++ script/script.js
@@ -1,46 +1,26 @@
-const toggleButton = document.getElementById("toggleButton")
-// const menu = document.getElementById("menu")
-const menuItems = document.querySelector("ul")
-let itemMenu = document.getElementsByClassName("menu-link")
-const menu = document.getElementById("menu")
-/* Listing Clicks */
-// document.addEventListener("click", menuLinkHandler)
-toggleButton.addEventListener("click", menuVisibility)
-document.addEventListener("scroll", scrollSpy)
+const toggleButton = document.getElementById("toggleButton") // SVG
+const menuUl = document.querySelector("ul")
+let itemMenu = document.getElementsByClassName("menu__link")
+const sections = document.querySelectorAll("header[data-scroll-spy], section[data-scroll-spy]")
 
+/*
+ * Listening the clicks
+ */
+toggleButton.addEventListener("click", menuVisibility)
+
+/*
+ * Menu
+ */
 for (let iterator of itemMenu) {
   iterator.addEventListener("click", menuLinkHandler)
 }
 
 function menuLinkHandler() {
-  menuItems.classList.toggle("menu-hidden")
+  menuUl.classList.toggle("menu-hidden")
   toggleButton.classList.toggle("active")
 }
 
 function menuVisibility() {
-  menuItems.classList.toggle("menu-visible")
-  menuItems.classList.toggle("menu-hidden")
-}
-
-function menuColor() {
-  document.documentElement.style.setProperty("--menu-color", "#e9c009")
-}
-
-function scrollSpy() {
-  const section = document.querySelectorAll("header[data-scroll-spy]")
-  // const section = document.querySelectorAll("section[data-scroll-spy]")
-
-  function callback(entries) {
-    entries.forEach((entry) => {
-      const elementoObserver = entry.target.getAttribute("id")
-      if (elementoObserver === "howToUse") {
-        console.log(elementoObserver)
-      }
-    })
-    // console.log(entries)
-  }
-
-  const observer = new IntersectionObserver(callback, {})
-
-  section.forEach((element) => observer.observe(element))
+  menuUl.classList.toggle("menu-visible")
+  menuUl.classList.toggle("menu-hidden")
 }
