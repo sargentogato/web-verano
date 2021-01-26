a264ee589341ac24e636026bcf44b11186e7f8b7
Marlon Velasquez <marlonjvelasquez@gmail.com>
2021-01-26 02:41:15 +0100

    script.js Add -Code for egg

--- script/script.js
+++ script/script.js
@@ -24,3 +24,28 @@ function menuVisibility() {
   menuUl.classList.toggle("menu-visible")
   menuUl.classList.toggle("menu-hidden")
 }
+
+/* egg */
+let position = 0
+const egg = document.getElementById("egg")
+function setTime() {
+  console.log("Función llamada")
+  for (let index = 1; index <= 7; index++) {
+    position = positionGenerator()
+    document.documentElement.style.setProperty(`--n${index}`, `${position}%`)
+    console.log(position)
+    console.log("variable:", index)
+  }
+  document.documentElement.style.setProperty("--egg", "display")
+  setTimeout(() => {
+    document.documentElement.style.setProperty("--egg", "none")
+  }, 10000)
+}
+
+egg.addEventListener("click", setTime)
+
+function positionGenerator() {
+  let counter = 0
+  console.log("llamda de la función ", counter++)
+  return Math.floor(Math.random() * 60)
+}
